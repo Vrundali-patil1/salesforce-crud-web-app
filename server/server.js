9bcdcc2161029cfd5cf5d3b9d48cec5857570d54
Vrundali Patil <vrundali@gmail.com>
2026-09-03 20:07:26 +0530

    Allow Vercel frontend in CORS

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ require("dotenv").config();
 const app = express();
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://salesforce-crud-web-app-one.vercel.app",
     credentials: true
 }));
 
